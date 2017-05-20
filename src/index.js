e925221e7548386a19f75fd949c93fdda0163efd
Tom Milligan <code@tommilligan.net>
2017-05-20 21:24:22 +0100

    Non-working, mid refactor to class and linting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,62 @@
-const getColors = require('get-image-colors')
+const getColors = require("get-image-colors");
+const GoogleImages = require("google-images");
 
 
-const getColorsFromJpgBuffer = (buffer) => {
-    return getColors(buffer, 'image/jpg');
-};
+class GetStringColors {
+    constructor(googleCseId, googleApiKey) {
+        this.googleImages = new GoogleImages(googleCseId, googleApiKey);
+    }
 
-const getStringColors = (queryString) => {
-    new Promise((resolve, reject) => {
-        // Get image url matching string
-        const imageUrls = getImageUrls(queryString);
-        // Download image into buffer
-        const buffer = curlImageToBuffer(imageUrls[0]);
-        // Get colors from image buffer
-        const colors = getColorsFromJpgBuffer(buffer);
-        // Return colors to fulfil promise
-        resolve(colors);
-        throw {name: "getStringColorsError", message: "General error"}
-    });
-};
+    requestJpgImageUrls (query, options={}) {
+        options = Object.assign({size: "medium"}, options);
+        this.googleImages.search(query, options)
+            .then(images => {
+                /*
+                [{
+                    "url": "http://steveangello.com/boss.jpg",
+                    "type": "image/jpeg",
+                    "width": 1024,
+                    "height": 768,
+                    "size": 102451,
+                    "thumbnail": {
+                        "url": "http://steveangello.com/thumbnail.jpg",
+                        "width": 512,
+                        "height": 512
+                    }
+                }]
+                */
+                return images[0].url;
+            });
+    }
 
-module.exports = getStringColors
+    requestImageAsBuffer (url) {
+        got(url)
+            .then(response => {
+                return response.body;
+            })
+            .catch(error => {
+                throw {name: error.response.body, message: `Failed requesting ${url} to buffer`};
+            });
+    }
+
+    getColorsFromJpgBuffer (buffer) {
+        return getColors(buffer, "image/jpg");
+    }
+
+    getStringColors (query) {
+        new Promise((resolve, reject) => {
+            // Get jpg image urls matching string
+            const imageUrls = this.requestJpgImageUrls(query);
+            // Download first image image into buffer
+            const buffer = this.requestImageUrlAsBuffer(imageUrls[0]);
+            // Get colors from image buffer
+            const colors = this.getColorsFromJpgBuffer(buffer);
+            // Return colors to fulfil promise
+            resolve(colors);
+            reject();
+            throw {name: "getStringColorsError", message: "General error"};
+        });
+    }
+}
+
+module.exports = GetStringColors;
--- test/test.js
+++ test/test.js
@@ -1,22 +1,41 @@
+const fs = require("fs")
+const path = require("path")
 
-const getStringColors = require('../src/index');
-const assert = require('chai').assert;
+const chai = require("chai");
+const assert = chai.assert;
+const expect = chai.expect;
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
 
-describe('Always', function() {
-  describe('pass', function() {
-    it('1 should equal 1', function() {
+const getStringColors = require("../src/index");
+
+const testDataDir = path.join(__dirname, "data")
+
+describe("Always", function () {
+  describe("pass", function () {
+    it("1 should equal 1", function () {
       assert.equal(1, 1);
     });
   });
 });
 
-describe('Module exports', function() {
-  describe('Default export', function() {
-    it('should be of type function', function() {
-      assert.isType(getStringColors, Function);
+describe("Module exports", function () {
+  describe("Default export", function () {
+    it("should be of type function", function () {
+      assert.typeOf(getStringColors, "function");
     });
-    it('should return a promise', function() {
-      assert.isTrue(getStringColors())
+  });
+});
+
+describe("Color from JPEG bufffer", function () {
+  describe("Should return a chroma color object", function () {
+    const buffer = fs.readFileSync(path.join(testDataDir, "orange_ff6600.jpg"))
+    it("resolves as promised", function() {
+        return expect(Promise.resolve("woof")).to.eventually.equal("woof");
+    });
+
+    it("rejects as promised", function() {
+        return expect(Promise.reject("caw")).to.be.rejectedWith("caw");
     });
   });
 });
